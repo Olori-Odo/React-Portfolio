659240e9abc6af903bf0b2759ace635fb6fa2add
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-08-08 19:45:59 +0100

    re-route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
 import "./App.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+// import { RouterProvider } from "react-router-dom";
+// import router from "./router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "../src/layout/MainLayout";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Project from "./pages/Project";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      {/* <RouterProvider router={router} /> */}
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainLayout />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
