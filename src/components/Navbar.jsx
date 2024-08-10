b1f69f15cff233011c3a1e8bc0abd32df35cd479
Fatai Akinyemi <ojuotimi04@gmail.com>
2024-08-10 10:54:59 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,59 @@
 import React from "react";
 import Logo from "../assets/dev-logo.png";
-import Links from "../layout/Links";
+// import Links from "../layout/Links";
 import { Outlet, Link } from "react-router-dom";
 import { CiMenuBurger } from "react-icons/ci";
-import { TiThMenuOutline } from "react-icons/ti";
+import { CiSearch } from "react-icons/ci";
+// import { TiThMenuOutline } from "react-icons/ti";
 
 const Navbar = () => {
-  const Link = [
-    { to: "/home", id: 1, label: "Home" },
-    { to: "/about", id: 2, label: "About" },
-    { to: "/project", id: 3, label: "Project" },
-    { to: "/contact", id: 4, label: "Contact" },
-  ];
+  // const Link = [
+  //   { to: "/home", id: 1, label: "Home" },
+  //   { to: "/about", id: 2, label: "About" },
+  //   { to: "/project", id: 3, label: "Project" },
+  //   { to: "/contact", id: 4, label: "Contact" },
+  // ];
   return (
     <>
       <nav>
         <img src={Logo} alt="Logo" />
-        <div className="nav-links">
+        {/* <div className="nav-links">
           {Link.map(({ to, label, id }) => (
             <Links className="links" to={to} key={id}>
               {label}
             </Links>
           ))}
-        </div>
+        </div> */}
+        <ul>
+          <li>
+            <Link to="/home" className="list">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about " className="list">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/project" className="list">
+              Project
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" className="list">
+              Contact
+            </Link>
+          </li>
+        </ul>
         <div className="search">
-          <p>Search</p>
+          <input type="text" placeholder="Search..." />
+          <CiSearch className="fasearch" />
         </div>
-        <CiMenuBurger />
-        <TiThMenuOutline />
+        <CiMenuBurger className="menu" />
+        {/* <TiThMenuOutline className="menu" /> */}
       </nav>
+      <hr />
       <Outlet />
     </>
   );
